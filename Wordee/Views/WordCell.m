8f1421eced9b27115220df1f50f8e8dd6f62ab96
Artur Wiśniewski <artwis21@gmail.com>
2020-09-02 12:49:45 +0200

    changed colors in Vocabulary view

--- Wordee/Views/WordCell.m
+++ Wordee/Views/WordCell.m
@@ -15,6 +15,19 @@
     // Initialization code
     self.externalView.layer.masksToBounds = YES;
     self.externalView.layer.cornerRadius = 10;
+//
+    self.externalView.layer.shadowColor = [UIColor grayColor].CGColor;
+    self.externalView.layer.shadowOffset = CGSizeMake(0, 3);
+    self.externalView.layer.shadowOpacity = 0.7;
+    self.externalView.layer.shadowRadius = 2.5;
+    self.externalView.clipsToBounds = NO;
+    self.externalView.backgroundColor = [UIColor systemBackgroundColor];
+    
+//    self.layer.shadowColor = [UIColor grayColor].CGColor;
+//    self.layer.shadowOffset = CGSizeMake(0, 3);
+//    self.layer.shadowOpacity = 0.7;
+//    self.layer.shadowRadius = 2.5;
+//    self.clipsToBounds = NO;
     
 }
 
